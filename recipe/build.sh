b43019357126c911d1ca1372dbee86b5cefd6f7b
Ohad Ravid <ohad.rv@gmail.com>
2019-02-27 14:09:21 +0100

    Remove commented out stuff

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,13 +9,6 @@ PYPY3_SRC_DIR=$SRC_DIR/pypy3
 if [ $(uname) == Darwin ]; then
     export CC=$CLANG
     export PYTHON=$SRC_DIR/pypy2-osx/bin/pypy
-
-    # libffi doesn't look in the correct location. We modify a copy of it since it's a hard link to conda's file.
-    # This is only relevant during the build, so we will put the original file back at the end.
-#    mv ${PREFIX}/lib/libffi.6.dylib ${PREFIX}/lib/libffi.6.dylib.bak
-#    cp ${PREFIX}/lib/libffi.6.dylib.bak ${PREFIX}/lib/libffi.6.dylib
-
-#    install_name_tool -id ${PREFIX}/lib/libffi.6.dylib ${PREFIX}/lib/libffi.6.dylib
 fi
 
 if [ $(uname) == Linux ]; then
@@ -73,9 +66,6 @@ if [ $(uname) == Darwin ]; then
     # Change @rpath to be relative to match conda's structure.
     ${INSTALL_NAME_TOOL} -rpath "${PREFIX}/lib" "@loader_path/../lib" $PREFIX/bin/pypy3
     rm $GOAL_DIR/${PKG_NAME}-c.bak
-
-    # The original libffi, works, so there is no need to ship our patched version.
-#    mv ${PREFIX}/lib/libffi.6.dylib.bak ${PREFIX}/lib/libffi.6.dylib
 fi
 
 
